f8fded70a3640ba164f0a8469b2b80c7a674e0ae
rakesh031986jha <rakesh031986jha@gmail.com>
2018-10-04 18:16:51 +0530

    add One more intent for Underwriting

--- index.js
+++ index.js
@@ -692,7 +692,7 @@ app.post("/fulfillment", async function (req, res) {
                              "messages": [{
                               "type": 0,
                               "platform": "facebook",
-                              "speech": "Your Claim number is CL  "+'<style.color =blue>'+claimno+'</style>'
+                              "speech": "Your Claim number is CL  "+claimno
                             },{
                               "type": 0,
                               "platform": "facebook",
